5bcefdc7c672fa9b90ea329fce57f3d2f13c8e38
huzj2 <huzj2@cmschina.com.cn>
2018-12-14 16:41:59 +0800

    support init several times

--- lib/android/src/main/java/cn/qiuxiang/react/baidumap/modules/BaiduMapInitializerModule.kt
+++ lib/android/src/main/java/cn/qiuxiang/react/baidumap/modules/BaiduMapInitializerModule.kt
@@ -4,6 +4,9 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import com.baidu.mapapi.SDKInitializer
 import com.baidu.mapapi.SDKInitializer.*
 import com.facebook.react.bridge.*
@@ -11,20 +14,37 @@ import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEm
 
 @Suppress("unused")
 class BaiduMapInitializerModule(private val context: ReactApplicationContext) : ReactContextBaseJavaModule(context) {
-    class SDKReceiver(promise: Promise) : BroadcastReceiver() {
-        private var promise: Promise? = promise
+
+    private val mCurrentReceiver: SDKReceiver by lazy { SDKReceiver() }
+
+    init {
+        Handler(Looper.getMainLooper()).post {
+            SDKInitializer.initialize(context.applicationContext)
+        }
+    }
+
+    class SDKReceiver : BroadcastReceiver() {
+
+        private val mPromiseList by lazy { arrayListOf<Promise>() }
 
         override fun onReceive(context: Context, intent: Intent) {
-            try {                
+            try {
                 if (intent.action == SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_OK) {
-                    promise?.resolve(null)
+                    mPromiseList.forEach { it.resolve(null) }
                 } else {
                     val code = intent.getIntExtra(SDK_BROADTCAST_INTENT_EXTRA_INFO_KEY_ERROR_CODE, 0)
-                    promise?.reject(code.toString(), intent.action)
+                    mPromiseList.forEach { it.reject(code.toString(), intent.action) }
                 }
+            } catch (e: Exception) {
+                Log.e("BaiduMapModule", "handle baidu message error", e)
+            } finally {
+                mPromiseList.clear()
             }
-            catch(Exception e) {
-            }
+
+        }
+
+        operator fun plusAssign(promise: Promise) {
+            mPromiseList.add(promise)
         }
     }
 
@@ -40,10 +60,22 @@ class BaiduMapInitializerModule(private val context: ReactApplicationContext) : 
 
     @ReactMethod
     fun init(promise: Promise) {
-        val intentFilter = IntentFilter()
-        intentFilter.addAction(SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_OK)
-        intentFilter.addAction(SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_ERROR)
-        context.currentActivity?.registerReceiver(SDKReceiver(promise), intentFilter)
-        SDKInitializer.initialize(context.applicationContext)
+        //
+
+        context.currentActivity?.apply {
+            val intentFilter = IntentFilter()
+            intentFilter.addAction(SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_OK)
+            intentFilter.addAction(SDK_BROADTCAST_ACTION_STRING_PERMISSION_CHECK_ERROR)
+            try {
+                unregisterReceiver(mCurrentReceiver)
+            } catch (e: Exception) {
+                Log.e("BaiduMapModule", "unregister failed", e)
+            }
+            registerReceiver(mCurrentReceiver, intentFilter)
+            mCurrentReceiver += promise
+        }
+
     }
+
+
 }
